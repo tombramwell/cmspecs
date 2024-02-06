3cc78f34c9db8d77adb81c937c05f0f73f52b4d5
Tom Bramwell <tom.bramwell@gmail.com>
2024-02-06 12:18:55 +0000

    updated logo

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className= "h-40" id="header">
         <div className={styles.container}>
 
-            <img className="h-40 p-4 w-150" src="https://res.cloudinary.com/monday-platform-eu/image/upload/v1696420425/board_views_images/logos/1696420424986_2b22ee35-6468-49bb-ebb7-83499bfe1c28.png" alt="Studio Logo" />
+            <img className="h-40 p-4 w-150" src="https://res.cloudinary.com/monday-platform-eu/image/upload/v1707221730/board_views_images/logos/1707221730243_d19d73be-047c-8f87-79ad-ab5818799acb.png" alt="Studio Logo" />
 
             <div className={`${styles.box} ${styles.lowered}`}>
             <p class={styles.text}>Content Marketing Specifications</p>
