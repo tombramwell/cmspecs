80b3f14a2f7117d57dda6af5ae2b46eb448dcc11
Tom Bramwell <tom.bramwell@gmail.com>
2024-03-06 15:13:06 +0000

    file

--- pages/form.js
+++ pages/form.js
@@ -33,7 +33,7 @@ export default function Form() {
           var blob = pdf.output();
           // window.open(URL.createObjectURL(new Blob([blob], { type: "application/pdf" })));
           var file = new File(blob, {name: fileName}, {type: "application/pdf"});
-          window.open(URL.createObjectURL(file));
+          window.open(file, '_blank');
 
       } else {
 
