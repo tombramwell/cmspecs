797ac34a39b05c0e67688837be9a3866758f3994
Tom Bramwell <tom.bramwell@gmail.com>
2024-03-27 15:14:39 +0000

    helper text

--- pages/form.js
+++ pages/form.js
@@ -85,11 +85,12 @@ export default function Form() {
     <div className="ml-10">
         <div id="bespokeForm" className="m-5">
         <div className="flex flex-column gap-2">
+          <br />
           <label htmlFor="businessName">Business name</label>
-          <InputText id="businessName" aria-describedby="businessName-help" value={businessName} onChange={(e) => setBusinessName(e.target.value)} />
           <small id="businessName-help">
               Provide your business name as it is to be referred to within the final article
           </small>
+          <InputText id="businessName" aria-describedby="businessName-help" value={businessName} onChange={(e) => setBusinessName(e.target.value)} />
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="industry">Industry</label>
@@ -97,10 +98,10 @@ export default function Form() {
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="targetAudience">Target audience</label>
-          <InputTextarea autoResize value={targetAudience} aria-describedby="targetAudience-help" onChange={(e) => setTargetAudience(e.target.value)} rows={5} cols={30} />
           <small id="targetAudience-help">
               What can you tell us about the people you want to target?
           </small>
+          <InputTextarea autoResize value={targetAudience} aria-describedby="targetAudience-help" onChange={(e) => setTargetAudience(e.target.value)} rows={5} cols={30} />
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="reasoning">Reason for advertising</label>
@@ -108,7 +109,6 @@ export default function Form() {
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="keyMessaging">Key messaging: What are the important things you want to say about your business?</label>
-          <InputTextarea autoResize value={keyMessaging} aria-describedby="keyMessaging-help" onChange={(e) => setKeyMessaging(e.target.value)} rows={7} cols={30} />
           <small id="keyMessaging-help">
               <ul>
                 <li>What are your USPs? Have you won any awards?</li>
@@ -117,13 +117,14 @@ export default function Form() {
                 <li>What about your business makes you most proud?</li>
               </ul>
           </small>
+          <InputTextarea autoResize value={keyMessaging} aria-describedby="keyMessaging-help" onChange={(e) => setKeyMessaging(e.target.value)} rows={7} cols={30} />
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="backgroundInfo">Please provide some background information about your business</label>
-          <InputTextarea autoResize value={backgroundInfo} aria-describedby="backgroundInfo-help" onChange={(e) => setBackgroundInfo(e.target.value)} rows={5} cols={30} />
           <small id="backgroundInfo-help">
             You can include business history, location(s), key directors&apos; or employees&apos; background, mission statements and anything else you feel it is important to include
           </small>
+          <InputTextarea autoResize value={backgroundInfo} aria-describedby="backgroundInfo-help" onChange={(e) => setBackgroundInfo(e.target.value)} rows={5} cols={30} />
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="eventsOffers">If applicable, are there any events or offers you&apos;d like us to cover?</label>
@@ -131,24 +132,24 @@ export default function Form() {
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="imagesVideo">What images and/or video are you providing?</label>
-          <InputTextarea autoResize value={imagesVideo} aria-describedby="imagesVideo-help" onChange={(e) => setImagesVideo(e.target.value)} rows={5} cols={30} />
           <small id="imagesVideo-help">
             Attach these elements to the email to your Reach sales contact. Here, please provide names and locations to be included in photo captions, along with any image credits that may need to be included
           </small>
+          <InputTextarea autoResize value={imagesVideo} aria-describedby="imagesVideo-help" onChange={(e) => setImagesVideo(e.target.value)} rows={5} cols={30} />
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="quotes">Please provide some words that you&apos;re happy for us to use as quotes to support the theme of the article</label>
-          <InputTextarea autoResize value={quotes} aria-describedby="quotes-help" onChange={(e) => setQuotes(e.target.value)} rows={5} cols={30} />
           <small id="quotes-help">
             This can be attributed to the business owner or a relevant employee. Please provide the name and job role of anyone to be named as the quote provider
           </small>
+          <InputTextarea autoResize value={quotes} aria-describedby="quotes-help" onChange={(e) => setQuotes(e.target.value)} rows={5} cols={30} />
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="referencePoints">Other helpful points of reference</label>
-          <InputTextarea autoResize value={referencePoints} aria-describedby="referencePoints-help" onChange={(e) => setReferencePoints(e.target.value)} rows={5} cols={30} />
           <small id="referencePoints-help">
             E.g. website, social media channels or previous articles that may be useful to form part of the writer&apos;s research
           </small>
+          <InputTextarea autoResize value={referencePoints} aria-describedby="referencePoints-help" onChange={(e) => setReferencePoints(e.target.value)} rows={5} cols={30} />
         </div>
         <div className="flex flex-column gap-2">
           <label htmlFor="additionalPoints">Is there anything else that hasn&apos;t been covered that you&apos;d like to be included in this article?</label>
